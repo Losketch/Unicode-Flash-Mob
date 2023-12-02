d498ccf7f6207fdf2b5d215f3e33c34c9d46d342
Losketch <140333245+Losketch@users.noreply.github.com>
2023-12-02 20:04:56 +0800

    Add files via upload

--- 生成mp4文件.py
+++ 生成mp4文件.py
@@ -17,9 +17,21 @@ def convert_images_to_video(image_folder, output_file, frame_rate, file_list):
     # 删除临时文件
     os.remove(temp_file)
 
-# 设置输入文件夹和输出文件路径
+# 获取输入文件夹和输出文件路径
 input_folder = 'png'  # 输入文件夹路径
-output_file = 'output/video.mp4'  # 输出视频文件路径
+
+# 获取用户输入的输出视频名称
+def get_output_video_name():
+    while True:
+        output_file_name = input("请输入输出视频的名称：")  # 自定义输出视频名称
+        output_file = os.path.join('output', output_file_name + '.mp4')  # 输出视频文件路径（自动添加路径和扩展名）
+        
+        if os.path.exists(output_file):
+            replace = input("已存在同名文件，是否替换？(Y/N): ")
+            if replace.lower() == 'y':
+                return output_file_name
+        else:
+            return output_file_name
 
 # 获取用户输入的视频帧率
 def get_frame_rate():
@@ -34,10 +46,13 @@ def get_frame_rate():
         except ValueError:
             print("无效的输入，请输入一个数字.")
 
+output_file_name = get_output_video_name()
+frame_rate = get_frame_rate()
+
 # 获取图像文件列表并按照用户要求的顺序排序
 image_files = [f for f in os.listdir(input_folder) if f.endswith('.png')]
 sorted_image_files = sorted(image_files, key=lambda x: int(x.split('_U+')[1].split('.')[0], 16))
 
 # 生成视频
-frame_rate = get_frame_rate()
+output_file = os.path.join('output', output_file_name + '.mp4')  # 输出视频文件路径
 convert_images_to_video(input_folder, output_file, frame_rate, sorted_image_files)
